bda00ffc6eedb0a94ca2c650d774a9524a41a9ce
wai123999 <tommy_6699045@hotmail.com>
2019-03-19 16:33:54 +0800

    change

---
+++ Position.ts
@@ -0,0 +1,14 @@
+class Vector{
+   posX : number;
+   posY : number;
+   constructor(){
+    this.posX = 0;
+    this.posY = 0;
+   }
+   public setX(n:number){
+      this.posX = n;
+   }
+   public setY(n:number){
+     this.posY = n;
+   }
+}
